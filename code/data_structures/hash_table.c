6fac5d98e346f278e1be4cae612d4e20d048ee51
lorrinho <contato.lorransoares@gmail.com>
2024-03-28 11:01:55 -0300

    feat: finished hash table using strings for keys and value

--- code/data_structures/hash_table.c
+++ code/data_structures/hash_table.c
@@ -12,8 +12,9 @@ HashTable *hash_table_create() {
 
 HashTableItem *hash_table_create_item(char *key, char *value) {
   HashTableItem *hash_item = (HashTableItem *)malloc(sizeof(HashTableItem));
-  hash_item->key = key;
-  hash_item->value = value;
+  hash_item->key = strdup(key);
+  hash_item->value = strdup(value);
+  hash_item->next = NULL;
   return hash_item;
 };
 
@@ -31,11 +32,32 @@ void hash_table_add(HashTable *hash_table, char *key, char *value) {
   }
 };
 
+void hash_table_remove(HashTable *hash_table, char *key) {
+  int index = hash(key);
+  HashTableItem *current = hash_table->items[index];
+  HashTableItem *previous = NULL;
+  while (current) {
+    if (strcmp(current->key, key) == 0) {
+      if (previous == NULL) {
+        hash_table->items[index] = current->next;
+      } else {
+        previous->next = current->next;
+      }
+      free(current->key);
+      free(current->value);
+      free(current);
+      return;
+    }
+    previous = current;
+    current = current->next;
+  }
+};
+
 bool hash_table_exists(HashTable *hash_table, char *key) {
   for (int i = 0; i < HASH_CAPACITY; i++) {
     HashTableItem *current = hash_table->items[i];
-    while (current != NULL) {
-      if (current->key && strcmp(current->key, key) == 0) {
+    while (current) {
+      if (strcmp(current->key, key) == 0) {
         return true;
       }
       current = current->next;
@@ -47,7 +69,7 @@ bool hash_table_exists(HashTable *hash_table, char *key) {
 char *hash_table_get(HashTable *hash_table, char *key) {
   int index = hash(key);
   HashTableItem *current = hash_table->items[index];
-  while (current != NULL) {
+  while (current) {
     if (strcmp(current->key, key) == 0) {
       return current->value;
     } else {
@@ -57,40 +79,19 @@ char *hash_table_get(HashTable *hash_table, char *key) {
   return "";
 };
 
-void hash_table_remove(HashTable *hash_table, char *key) {
-  int index = hash(key);
-  HashTableItem *current = hash_table->items[index];
-  HashTableItem *previous = NULL;
-  while (current) {
-    if (strcmp(hash_table->items[index]->key, key) == 0) {
-      if (previous == NULL) {
-        hash_table->items[index] = current->next;
-      } else {
-        previous->next = current->next;
-      }
-      free(current->key);
-      free(current->value);
-      free(current);
-    }
-    previous = current;
-    current = current->next;
-  }
-};
-
 void hash_table_destroy(HashTable *hash_table) {
   for (int i = 0; i < HASH_CAPACITY; i++) {
     HashTableItem *current = hash_table->items[i];
-    while (current != NULL) {
+    while (current) {
       HashTableItem *temp = current;
       current = current->next;
-      if (temp->key)
-        free(temp->key);
-      if (temp->value)
-        free(temp->value);
+      free(temp->key);
+      free(temp->value);
       free(temp);
     }
     hash_table->items[i] = NULL;
   }
+  free(hash_table->items);
   free(hash_table);
 };
 
@@ -107,7 +108,8 @@ void test_hash_table(HashTable *hash_table) {
   char *value1 = "world!";
   hash_table_add(hash_table, key1, value1);
   printf("value of the key \"hello\": %s\n", hash_table_get(hash_table, key1));
-  hash_table_remove(hash_table, key1);
   printf("does \"hello\" exists?: %i\n", hash_table_exists(hash_table, key1));
+  hash_table_remove(hash_table, key1);
+  printf("does \"hello\" exists after removal?: %i\n", hash_table_exists(hash_table, key1));
   hash_table_destroy(hash_table);
 }
