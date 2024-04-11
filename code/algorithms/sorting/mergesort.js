26966ea038f7662d04539ca3670b49ba3ca7b756
lorrinho <contato.lorransoares@gmail.com>
2024-04-11 18:06:35 -0300

    feat: adding merge sort implementation in javascript

--- code/algorithms/sorting/mergesort.js
+++ code/algorithms/sorting/mergesort.js
@@ -1,3 +1,32 @@
-function mergesort() {
-  // TODO
+function merge(arrLeft, arrRight) {
+  let finalArray = [];
+  while (arrLeft.length !== 0 && arrRight.length !== 0) {
+    if (arrLeft[0] <= arrRight[0]) {
+      finalArray.push(arrLeft.shift());
+    } else {
+      finalArray.push(arrRight.shift());
+    }
+  }
+  return finalArray.concat(arrLeft, arrRight);
 }
+
+function mergeSort(arr) {
+  if (arr.length === 1) return arr;
+  let [arrLeft, arrRight] = [
+    arr.slice(0, arr.length / 2),
+    arr.slice(arr.length / 2),
+  ];
+  arrLeft = mergeSort(arrLeft);
+  arrRight = mergeSort(arrRight);
+  return merge(arrLeft, arrRight);
+}
+
+let testArray = [6, 3, 28, 19, 27, 25, 12];
+console.log(mergeSort(testArray).join(" -> "));
+
+/*
+ * Basically splits the array into smallers arrays until there are only arrays with two elements
+ * this makes comparing the elements after sorting the previous arrys better, because
+ * as example, these two, after being initially sorted, [1, 2] & [4, 5] only need to have two comparisons
+ * to create an array with 4 elements [1, 2, 4, 5], then, this can be compared to another with 4, etc.
+ */
